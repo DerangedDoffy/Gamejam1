2478e367f6b460a417cf81d184af91ab3c9cdf8a
DerangedDoffy <matthew.mirfield@gmail.com>
2022-07-10 23:40:09 -0400

    starting levels, improved movement

--- gamejamParkour/Assets/Scripts/PlayerMovement.cs
+++ gamejamParkour/Assets/Scripts/PlayerMovement.cs
@@ -97,7 +97,7 @@ public class PlayerMovement : MonoBehaviour
     {
         if (isGrounded)
         {
-            rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+            rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
             rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
         }
     }
